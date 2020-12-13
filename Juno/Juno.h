cb4395173221069ffb8f3377d414de2340588bd1
Emre Onder <e.onder@papara.com>
2020-12-13 11:25:11 +0300

    Initial Commit

---
+++ Juno/Juno.h
@@ -0,0 +1,18 @@
+//
+//  Juno.h
+//  Juno
+//
+//  Created by Emre Onder on 13.12.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Juno.
+FOUNDATION_EXPORT double JunoVersionNumber;
+
+//! Project version string for Juno.
+FOUNDATION_EXPORT const unsigned char JunoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Juno/PublicHeader.h>
+
+
